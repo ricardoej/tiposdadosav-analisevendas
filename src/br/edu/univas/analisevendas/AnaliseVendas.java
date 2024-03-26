36d516e0e9b6158a52fe05c14bc1b8f58a77ef7a
unknown <Administrador@LABF241349.fuvs.br>
2024-03-25 21:36:10 -0300

    Ricardo Emerson Julio

---
+++ src/br/edu/univas/analisevendas/AnaliseVendas.java
@@ -0,0 +1,82 @@
+package br.edu.univas.analisevendas;
+
+public class AnaliseVendas {
+	
+	int[][] dadosVendas;
+	
+	public AnaliseVendas(int[][] minhaMatriz) {
+		dadosVendas = minhaMatriz;
+	}
+	
+	
+	public int calcularTotalVendasProduto(int produto) {
+		int[] vetorDoProduto = dadosVendas[produto];
+		int total = 0;
+		for (int i = 0; i < vetorDoProduto.length; i++) {
+			total = total + vetorDoProduto[i];
+		}
+		
+		return total;
+	}
+
+
+	public int identificarMesMaiorVendas() {
+		int maiorValorVendas = Integer.MIN_VALUE;
+		int mesMaiorValor = 0;
+		for(int i = 0; i < dadosVendas[0].length; i++) {
+			int somaDoMesI = 0;
+			for( int j = 0; j < dadosVendas.length; j++) {
+				somaDoMesI += dadosVendas[j][i];
+			}
+			if(somaDoMesI > maiorValorVendas) {
+				mesMaiorValor = i+1;
+				maiorValorVendas = somaDoMesI;
+			}
+		}
+		return mesMaiorValor;
+	}
+
+
+	public double calcularMediaVendasMensais(int produto) {
+		double soma = 0;
+		for (int i = 0; i < dadosVendas[produto].length  ; i++) {
+			soma += dadosVendas[produto][i];
+		}
+		double media = soma /dadosVendas[produto].length;
+		return media;
+	}
+
+
+	public int identificarProdutoMaisVendido() {
+		int produtoMaiorValor = 0;
+		double maiorValor= Double.MIN_VALUE;
+		for(int i=0;i<dadosVendas.length;i++) {
+			int soma=0;
+			for(int j=0;j<dadosVendas[i].length;j++) {
+				soma += dadosVendas[i][j];
+			}
+			if(soma > maiorValor) {
+				maiorValor = soma;
+				produtoMaiorValor = i;
+			}
+		}
+		return produtoMaiorValor;
+	}
+
+
+	public int identificarMesMenorVendas() {
+		int menorValorVendas = Integer.MAX_VALUE;
+		int mesMenorValor = 0;
+		for(int i = 0; i < dadosVendas[0].length; i++) {
+			int somaDoMesI = 0;
+			for( int j = 0; j < dadosVendas.length; j++) {
+				somaDoMesI += dadosVendas[j][i];
+			}
+			if(somaDoMesI < menorValorVendas) {
+				mesMenorValor = i+1;
+				menorValorVendas = somaDoMesI;
+			}
+		}
+		return mesMenorValor;
+	}
+}
